d797ccec48582d4c73bb4e184801973c74fa6298
Alexander Egorov <avegrv@gmail.com>
2019-11-20 17:58:51 +0400

    [Chat] fix set not null

--- src/reducers/entities/channels.ts
+++ src/reducers/entities/channels.ts
@@ -24,7 +24,7 @@ function channelListToSet(state: any, action: GenericAction) {
 
     action.data.forEach((channel: Channel) => {
         const set = nextState[channel.team_id];
-        if (Object.keys(set).length !== 0) {
+        if (Boolean(set) && Object.keys(set).length !== 0) {
             const nextSet = new Set(nextState[channel.team_id]);
             nextSet.add(channel.id);
             nextState[channel.team_id] = nextSet;
